70ff8abd194341f54b970bbef79621937a176d1f
André Macena <andrmacena_@outlook.com>
2019-07-09 19:56:25 -0300

    Alterado App.js
    Foi adicionado mais algumas chamadas do componente Field, passando algumas propriedades para renderização em tela

--- App.js
+++ App.js
@@ -11,12 +11,13 @@ export default class App extends Component {
             <Text style={styles.instructions}>Tamanho da grade:
              {params.getRowsAmount()}x{params.getColumnsAmount()} </Text>
             <Field />
-            <Field opened/>
-            <Field opened nearMines={1}/>
-            <Field opened nearMines={2}/>
-            <Field opened nearMines={3}/>
-            <Field opened nearMines={6}/>
-            <Field opened nearMines={6}/>
+            <Field opened />
+            <Field opened nearMines={1} />
+            <Field opened nearMines={2} />
+            <Field opened nearMines={3} />
+            <Field opened nearMines={6} />
+            <Field opened mined />
+            <Field opened mined exploded />
          </View>
       )
    }
